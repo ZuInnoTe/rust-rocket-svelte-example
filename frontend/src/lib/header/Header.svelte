3c6096e2bb944eab7ff3ca3b8ecda45987bacee9
Jörn Franke <jornfranke@gmail.com>
2024-08-04 01:31:24 +0200

    init

---
+++ frontend/src/lib/header/Header.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import TopAppBar, { Row, Section, Title, AutoAdjust } from '@smui/top-app-bar';
+	import Fab, { Icon } from '@smui/fab';
+	import IconButton from '@smui/icon-button';
+
+	let topAppBar: TopAppBar;
+
+	function navigate(route: string, replaceState: boolean) {
+		goto(`/${route}`, { replaceState });
+	}
+</script>
+
+<TopAppBar bind:this={topAppBar} variant="standard">
+	<Row>
+		<Section>
+			<IconButton
+				class="material-icons"
+				aria-label="Inventory"
+				on:click={() => navigate('inventory', false)}>inventory</IconButton
+			>
+			<IconButton
+				class="material-icons"
+				aria-label="Shopping Cart"
+				on:click={() => navigate('order', false)}>shopping_cart</IconButton
+			>
+			<Title>MyApp</Title>
+		</Section>
+		<Section align="end" toolbar>
+			<IconButton class="material-icons" aria-label="Settings">settings</IconButton>
+		</Section>
+	</Row>
+</TopAppBar>
+<AutoAdjust {topAppBar}></AutoAdjust>
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { assets } from '$app/paths';
+	import Header from '../lib/header/Header.svelte';
+</script>
+
+<svelte:head>
+	<!-- SMUI Styles -->
+	<link rel="stylesheet" href="{assets}/css/smui.css" />
+
+	<!-- Site Styles -->
+	<link rel="stylesheet" href="{assets}/css/site.css" />
+
+	<!-- Site Styles -->
+	<link rel="stylesheet" href="{assets}/css/site.css" media="(prefers-color-scheme: light)" />
+	<link
+		rel="stylesheet"
+		href="{assets}/css/site-dark.css"
+		media="screen and (prefers-color-scheme: dark)"
+	/>
+</svelte:head>
+
+<Header />
+
+<slot />
