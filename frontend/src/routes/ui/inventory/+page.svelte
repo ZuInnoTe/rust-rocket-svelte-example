676ab9fa884f0c001a6e5a96bc6acff2250a60f3
Jörn Franke <jornfranke@gmail.com>
2024-10-03 22:48:19 +0200

    init

---
+++ frontend/src/routes/ui/inventory/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Inventory from '../../../lib/inventory/Inventory.svelte';
+</script>
+
+<Inventory></Inventory>
---
+++ frontend/src/routes/ui/order/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Order from '../../../lib/order/Order.svelte';
+</script>
+
+<Order></Order>
